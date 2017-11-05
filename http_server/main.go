93666b11a554d5cd3328a891a8aa6e66824fcefb
teng <wwwsousoucom@126.com>
2017-11-05 19:54:45 +0800

    http_server

--- http_server/main.go
+++ http_server/main.go
@@ -25,11 +25,17 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+//func handler(w http.ResponseWriter, r *http.Request)  {
+//	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+//}
+
+func viewHandler(w http.ResponseWriter, r *http.Request)  {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	http.ListenAndServe(":3000", nil)
 }
