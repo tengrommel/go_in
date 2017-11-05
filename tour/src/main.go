0fe953d79b7e57c5799345174864fbe825d5b836
teng <wwwsousoucom@126.com>
2017-11-05 13:22:51 +0800

    println

--- tour/src/main.go
+++ tour/src/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 )
 
+var c, python, java bool
 var x, y, z float64
 var str string
 
 
 func main() {
 	fmt.Printf("%v, %v, %v, %q\n",x, y, z, str)
+	fmt.Println(c, python, java)
 }
